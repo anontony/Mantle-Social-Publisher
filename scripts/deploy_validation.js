5481bf1f3209d513fd270d447354c8a11bf13352
Tony CIG <85830442+anontony@users.noreply.github.com>
2026-05-30 16:55:10 +0700

    Add deploy script for BlockScamValidationRegistry

---
+++ scripts/deploy_validation.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying BlockScamValidationRegistry with:", deployer.address);
+  console.log("Network:", hre.network.name);
+
+  const Registry = await hre.ethers.getContractFactory("BlockScamValidationRegistry");
+  const registry = await Registry.deploy();
+
+  await registry.waitForDeployment();
+
+  const address = await registry.getAddress();
+
+  console.log("BlockScamValidationRegistry deployed to:", address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
